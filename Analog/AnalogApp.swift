e4a6fbfe9eefc4cbf220eef05f4680b12dd685bd
ZainMayoof <Zainaqeelmayoof@gmail.com>
2025-04-16 21:16:11 +0300

    Initial Commit

---
+++ Analog/AnalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalogApp.swift
+//  Analog
+//
+//  Created by zain Mayoof on 16/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct AnalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Analog/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Analog
+//
+//  Created by zain Mayoof on 16/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
